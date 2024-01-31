8a2be484f9f05cb8425036434ca1ffb50b730dfa
Vladyslav Lukiianchuk <vladlukiyanchukoo@gmail.com>
2024-01-31 16:55:25 +0200

    monitor

--- components/Monitor.vue
+++ components/Monitor.vue
@@ -1,30 +1,43 @@
 <script lang="ts" setup>
-
+  const enum RewardsFilters {
+    HOME = 0,
+    STAGES = 1,
+    PRICE = 2,
+    CONTACTS = 3
+  }
+  
+  const rewardFilters = ["Home", "Stages", "Price", "Contacts"]
+  const selectedFilter = ref(RewardsFilters.HOME)
 </script>
 
 <template>
   <section class="background">
     <v-container class="d-flex justify-center container-main px-0" fluid>
       <v-card :height="584" :max-width="1043" class="bg-white w-100 pa-4 monitor-border mt-12" rounded="lg">
-        <v-responsive class="bg-white h-100 px-16 pt-5">
+        <v-responsive class="bg-white h-100 px-8 px-lg-16 pt-5">
           <v-row no-gutters>
-
-            <v-col cols="12" class="mb-8">
-              <v-row align="center" >
+            
+            <v-col class="mb-8" cols="12">
+              <v-row align="center">
                 <v-col>
                   <span class="fz-24 font-weight-medium">LULU</span>
                 </v-col>
-                <v-col class="d-flex justify-space-between" cols="7" md="5">
-                  <v-btn class="fz-18 font-weight-medium text-capitalize px-0 ls-normal" variant="text">Home</v-btn>
-                  <v-btn class="fz-18 font-weight-medium text-capitalize px-0 ls-normal" variant="text">Stages</v-btn>
-                  <v-btn class="fz-18 font-weight-medium text-capitalize px-0 ls-normal" variant="text">Price</v-btn>
-                  <v-btn class="fz-18 font-weight-medium text-capitalize px-0 ls-normal" variant="text">Contacts</v-btn>
+                <v-col cols="6" md="5">
+                  <v-item-group v-model="selectedFilter" mandatory>
+                    <v-row class="justify-space-between" dense>
+                      <v-col v-for="(filter, index) in rewardFilters" :key="index" class="flex-0-0 fz-20" cols="auto">
+                        <v-item v-slot="{ isSelected, toggle }" :value="filter">
+                          <span :class="isSelected ? 'text-deep-purple-accent-4' : ''" class="cursor-pointer" @click="toggle">{{ filter }}</span>
+                        </v-item>
+                      </v-col>
+                    </v-row>
+                  </v-item-group>
                 </v-col>
               </v-row>
             </v-col>
             
             <v-col class="d-flex flex-column align-center h-100" cols="12">
-              <v-img src="/public/img/monitor-lulu.png" :max-width="539" class="mb-8 w-100"/>
+              <v-img :max-width="539" class="mb-8 w-100" src="/public/img/monitor-lulu.png"/>
               <span class="fz-32 mb-5">Studio</span>
               <span class="fz-18">Where Design Meets Function</span>
             </v-col>
@@ -45,5 +58,9 @@
   
   .monitor-border {
     background-color: #373738 !important;
+  }
+  
+  menu-btn {
+    color:
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,6 @@
 <template>
   <div>
     <Monitor/>
-    <p>fsdf</p>
-    <p>fsdf</p>
-    <p>fsdf</p>
-    <p>fsdf</p>
   </div>
 </template>
 
