2c6b900b8b89ea0ed74a1cf962678969538f5594
Vladyslav Lukiianchuk <vladlukiyanchukoo@gmail.com>
2024-03-05 16:12:57 +0200

    form

---
+++ pages/thanks.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class="h-100 pt-15">
+    <!--<img height="1" width="1" style="display:none" alt="" src="https://www.facebook.com/tr?id=285706427331877&ev=Lead&noscript=1" />-->
+    <v-row class="h-100" align="center" justify="center">
+      <v-col cols="12" md="8">
+        <fieldset class="rounded-xl">
+        <v-card height="100%" variant="text" class="bg-white neon-border ">
+          <v-card-text class="text-center py-15">
+            <h2 class="mb-4">Thank you for your order!</h2>
+            <p class="mb-4">We will contact you shortly.</p>
+            <nuxt-link class="link border px-4  py-2 d-inline-block text-black"  to="/">Back to Site</nuxt-link>
+          </v-card-text>
+        </v-card>
+        </fieldset>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style>
+
+</style>
