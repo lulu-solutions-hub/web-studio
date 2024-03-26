4391e7fc6a75e2f8e1dc73854d32e9d45b3cf31d
Vladyslav Lukiianchuk <vladlukiyanchukoo@gmail.com>
2024-03-26 16:34:34 +0200

    sitemap

--- components/Monitor.vue
+++ components/Monitor.vue
@@ -43,6 +43,24 @@
   });
 
   const burgerClick = ref(false)
+
+  useHead({
+    title: "LULU Web Studio",
+    meta: [
+      {
+        name: 'description',
+        content: 'Learn about LULU Web Studio, a team of specialists dedicated to creating unique websites using advanced technologies. We aim to maximize the success of our clients’ businesses.'
+      },
+      {
+        name: 'keywords',
+        content: 'LULU Web Studio, web design, specialist team, advanced technologies, business development, Internet advertising, website creation'
+      },
+      {
+        name: "google-site-verification",
+        content: "Rwkp0cpo7uQ4uim7VwevGQlh7zqG8ZkYndj0rb7CKRc"
+      },
+    ]
+  })
 </script>
 
 <template>
